2a9248d49828523b3692248036c2499365323144
takd <takacs.donat01@gmail.com>
2018-06-06 12:23:09 +0200

    Collected dependencies as of 2018. 06. 06.

---
+++ dependencies.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-setuptools python3-distutils-extra python3-cairo python3-cairo-dev pkg-config zbar-tools python3-reportlab texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra texlive-science libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev libtiff5-dev libglib2.0-dev python-zbar gir1.2-gtk-3.0 pdftk python-pypdf python-reportlab python-imaging python-setuptools
