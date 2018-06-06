e66f1178fca37bb132f066a7e6a8627884ff26ba
takd <takacs.donat01@gmail.com>
2018-06-06 18:12:17 +0200

    Fixed CSV script

--- get_csv.sh
+++ get_csv.sh
@@ -1,8 +1,8 @@
 if [ ! -d "_csv" ]; then
 mkdir _csv
 fi
-for d in ./_projektek/*/ ; do
+for d in _projektek/* ; do
     echo "mappa: $d"
-    python ./sdaps.py "${d::-1}" csv export
-    #mv "${d::-1}/data_1.csv" "./_csv/${d::-1}.csv"
+    ./sdaps.py "$d" csv export
+    mv "$d/data_1.csv" "./_csv/${d#*/}.csv"
 done
